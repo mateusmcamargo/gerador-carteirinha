6477dd3f0550b84ceadc6b485f07ee3d50ea8175
mateus <mateuscamargo2908@gmail.com>
2025-05-10 16:21:18 -0300

    finishing up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import QRCode from 'qrcode';
 import JsBarcode from 'jsbarcode';
 
 function App() {
-  const [textoQR, setTextoQR] = useState('');
+  const [textoQR,    setTextoQR]    = useState('');
   const [textoBarra, setTextoBarra] = useState('');
-  const [usarQR, setUsarQR] = useState(true);
-  const [usarBarra, setUsarBarra] = useState(false);
-  const [gerado, setGerado] = useState(false);
+  const [usarQR,     setUsarQR]     = useState(true);
+  const [usarBarra,  setUsarBarra]  = useState(false);
+  const [gerado,     setGerado]     = useState(false);
+
+  const [nome,         setNome]       = useState('');
+  const [curso,        setCurso]      = useState('');
+  const [validade,     setValidade]   = useState('');
+
   const canvasRef = useRef();
 
   const [userPicture, setProfilePicture] = useState(null);
@@ -28,28 +33,30 @@ function App() {
 
   const gerarImagem = async () => {
     try {
-      const fundo = new Image();
-      fundo.src = 'https://mateusmcamargo.github.io/code-generator/img.svg';
+      const fundo  = new Image();
+      fundo.src    = 'https://mateusmcamargo.github.io/code-generator/carteirinha_em_branco.svg';
+      fundo.width  = 1430;
+      fundo.height = 904;
       await fundo.decode();
   
       const finalCanvas = canvasRef.current;
       const ctx = finalCanvas.getContext('2d');
-      finalCanvas.width = fundo.width;
+      finalCanvas.width  = fundo.width;
       finalCanvas.height = fundo.height;
-      ctx.drawImage(fundo, 0, 0);
+      ctx.drawImage(fundo, 0, 0, fundo.width, fundo.height);
   
       if (usarQR && textoQR.trim()) {
         const qrCanvas = document.createElement('canvas');
         await QRCode.toCanvas(qrCanvas, textoQR, {
-          width: 204,
+          width: 285,
           margin: 0,
           color: {
             light: '#F6F6ED',
             dark: '#333333'
           }
         });
-        const posX = finalCanvas.width - qrCanvas.width - 63;
-        const posY = finalCanvas.height - qrCanvas.height - 277;
+        const posX = finalCanvas.width  - qrCanvas.width  - 94;
+        const posY = finalCanvas.height - qrCanvas.height - 396;
         try {
           ctx.drawImage(qrCanvas, posX, posY);
         } catch (err) {
@@ -63,16 +70,16 @@ function App() {
         try {
           JsBarcode(barcodeCanvas, textoBarra, {
             format: 'CODE128',
-            width: 7.68,
-            height: 82,
+            width: 10.74,
+            height: 114,
             displayValue: false,
             fit: false,
             margin: 0,
             background: '#F6F6ED',
             lineColor: '#333333'
           });
-          const posX = finalCanvas.width - barcodeCanvas.width - 63;
-          const posY = finalCanvas.height - barcodeCanvas.height - 139;
+          const posX = finalCanvas.width  - barcodeCanvas.width  - 95;
+          const posY = finalCanvas.height - barcodeCanvas.height - 203;
           ctx.drawImage(barcodeCanvas, posX, posY);
         } catch (err) {
           console.error('Erro ao desenhar código de barras:', err);
@@ -100,13 +107,52 @@ function App() {
           cropY = (img.height - cropHeight) / 2;
         }
 
+        ctx.fillStyle = '#333'; // cor do texto
+        ctx.textAlign = 'left';
+
+        const textXStart        = 488;
+        const textYName         = 254;
+        const textYNameMargin   = 68;
+        const textYUni          = textYName + textYNameMargin;
+        const textYMargin       = 52;
+
+        // Nome (negrito)
+        ctx.font = 'bold 34px sans-serif';
+        ctx.fillText(nome, textXStart, textYName); // X, Y
+
+        // Universidade
+        ctx.font = '34px sans-serif';
+        ctx.fillText('Universidade Tecnológica Federal', textXStart, textYUni);
+
+        // Curso
+        ctx.fillText(curso, textXStart, textYUni + (textYMargin * 1));
+
+        // Validade (negrito)
+        ctx.font = 'bold 34px sans-serif';
+        ctx.fillText('Data de Validade: ', textXStart, textYUni + (textYMargin * 2));
+        const textWidthDate = ctx.measureText('Data de Validade: ').width;
+
+        // Validade (valor)
+        ctx.font = '34px sans-serif';
+        ctx.fillText('31/12/2025', textXStart + textWidthDate, textYUni + (textYMargin * 2));
+
+        // Matrícula (negrito)
+        ctx.font = 'bold 34px sans-serif';
+        ctx.fillText('Matrícula: ', textXStart, textYUni + (textYMargin * 3));
+        const textWidthRegistration = ctx.measureText('Matrícula: ').width;
+
+        // Matrícula (valor)
+        ctx.font = '34px sans-serif';
+        ctx.fillText(textoQR, textXStart + textWidthRegistration, textYUni + (textYMargin * 3));
+
+
         // Agora você pode desenhar no canvas:
         ctx.drawImage(
           img,
           cropX, cropY,
           cropWidth, cropHeight,
-          64, 159,          // posição no canvas
-          256, 341 // tamanho desejado final (ex: 90 x 120)
+          98, 224, // posição no canvas
+          358, 477 // tamanho desejado final (ex: 90 x 120)
         );
       }
 
@@ -133,16 +179,6 @@ function App() {
           accept="image/*"
           onChange={handleProfileImage}
         />
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            checked={usarQR}
-            onChange={() => setUsarQR(!usarQR)}
-          />{' '}
-          incluir matricula
-        </label>
-        <br />
         <input
           type="text"
           placeholder="matricula"
@@ -153,15 +189,6 @@ function App() {
       </div>
 
       <div style={{ marginBottom: '1rem' }}>
-        <label>
-          <input
-            type="checkbox"
-            checked={usarBarra}
-            onChange={() => setUsarBarra(!usarBarra)}
-          />{' '}
-          incluir RA
-        </label>
-        <br />
         <input
           type="text"
           placeholder="RA"
@@ -171,13 +198,32 @@ function App() {
         />
       </div>
 
+      <div style={{ marginBottom: '1rem' }}>
+        <input
+          type="text"
+          placeholder="Nome do aluno"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          style={{ padding: '0.5rem', width: '60%', marginTop: '0.5rem' }}
+        />
+        <select
+          value={curso}
+          onChange={(e) => setCurso(e.target.value)}
+          style={{ padding: '0.5rem', width: '60%', marginTop: '0.5rem' }}
+        >
+          <option value="">Selecione o curso</option>
+          <option value="Engenharia de Computação">Engenharia de Computação</option>
+          <option value="Engenharia de Software">Engenharia de Software</option>
+          <option value="Análise e Desenv. de Sistemas">Análise e Desenv. de Sistemas</option>
+          <option value="Licenciatura em Matemática">Licenciatura em Matemática</option>
+        </select>
+      </div>
+
       <button onClick={gerarImagem} style={{ padding: '0.5rem 1rem' }}>
         gerar carteirinha
       </button>
-      <br /><br />
 
       <canvas ref={canvasRef} style={{ border: '1px solid #ccc' }}></canvas>
-      <br /><br />
 
       {gerado && (
         <button onClick={baixarImagem} style={{ padding: '0.5rem 1rem' }}>
